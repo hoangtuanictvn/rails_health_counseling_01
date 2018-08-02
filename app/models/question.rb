240b10d3e5ff992be22c32c0a92094997259383c
Hoang Tuan <hoangtuanictvn@gmail.com>
2018-08-02 10:12:26 +0700

    Add fulltext search

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,16 @@
 class Question < ApplicationRecord
+  include PgSearch
   belongs_to :user
   has_many :likes, as: :target
   has_many :answers, dependent: :destroy
   has_many :question_categories, foreign_key: :question_id, dependent: :destroy
   has_many :categories, through: :question_categories, source: :major
+
+  pg_search_scope :search,
+    against: [:title, :content],
+    using: {
+      tsearch: {
+        prefix: true
+      }
+    }
 end
