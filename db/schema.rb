27339234fb3326400112b43b320afa3345f7c3c8
Nguyễn Thành Nhân <nhannt.bk@gmail.com>
2018-08-03 16:33:42 +0700

    Like question and comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_043321) do
+ActiveRecord::Schema.define(version: 2018_08_03_021139) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2018_08_02_043321) do
     t.string "target_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "majors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
